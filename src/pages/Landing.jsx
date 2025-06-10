8d9625d5d810b5e138a4b564a1d30b70fe497515
SwarnabhaBiswas <swarnabha8481@gmail.com>
2025-06-10 11:50:36 +0530

    Initiated tailwind

--- src/pages/Landing.jsx
+++ src/pages/Landing.jsx
@@ -4,8 +4,8 @@ export default function Landing() {
   const navigate = useNavigate();
 
   return (
-    <div>
-      <h2>Welcome! Choose input method</h2>
+    <div className='flex flex-col items-center'>
+      <h2 >Welcome! Choose input method</h2>
       <button onClick={() => navigate('/manual')}>Enter Menu Manually</button>
       <button onClick={() => navigate('/upload-pdf')}>Upload PDF</button>
     </div>
