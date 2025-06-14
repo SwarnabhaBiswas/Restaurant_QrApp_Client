bacb0b1a364842bae3487e9df916680413433905
Nishchal Singh <nishchalsingh9a@gmail.com>
2025-06-15 00:02:05 +0530

    add components

---
+++ src/Components/Footer.jsx
@@ -0,0 +1,7 @@
+export default function Footer() {
+  return (
+    <footer className="bg-primary text-tertiary text-center py-4">
+      <p>&copy; {new Date().getFullYear()} MenuQR. All rights reserved.</p>
+    </footer>
+  );
+}
