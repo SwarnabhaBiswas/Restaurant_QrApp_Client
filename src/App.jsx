ca320af9e4f70363240ceeaccd91c9b8aeb91065
Nishchal Singh <nishchalsingh9a@gmail.com>
2025-06-14 23:58:40 +0530

    add templates

--- src/App.jsx
+++ src/App.jsx
@@ -5,18 +5,25 @@ import ManualInput from './pages/ManualInput';
 import UploadPDF from './pages/UploadPDF';
 import TemplateSelect from './pages/TemplateSelect';
 import Preview from './pages/Preview';
+import Navbar from './Components/Nav';
+import TemplateChooser from './pages/TemplateChooser';
+import MenuBuilder from './pages/MenuBuilder';
 
 export default function App() {
-
   const [pdfUrl, setPdfUrl] = useState('');
 
   return (
-    <Routes>
-      <Route path="/" element={<Landing />} />
-      <Route path="/manual" element={<ManualInput />} />
-      <Route path="/upload-pdf" element={<UploadPDF setPdfUrl={setPdfUrl} />} />
-      <Route path="/template" element={<TemplateSelect />} />
-      <Route path="/preview" element={<Preview />} />
-    </Routes>
+    <>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Landing />} />
+        <Route path="/manual" element={<ManualInput />} />
+        <Route path="/upload-pdf" element={<UploadPDF setPdfUrl={setPdfUrl} />} />
+        <Route path="/template" element={<TemplateSelect />} />
+        <Route path="/preview" element={<Preview />} />
+        <Route path="/menu-templates" element={<TemplateChooser />} />
+        <Route path="/menu-builder" element={<MenuBuilder />} />
+      </Routes>
+    </>
   );
 }
