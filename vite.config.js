6657af951f6f633f23f8ada5b8effdf54492156a
SwarnabhaBiswas <swarnabha8481@gmail.com>
2025-06-26 22:24:57 +0530

    config

--- vite.config.js
+++ vite.config.js
@@ -2,12 +2,7 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
-export default defineConfig({
-   server: {
-      allowedHosts: ['2d2a-45-251-234-179.ngrok-free.app'], 
-    proxy: {
-      '/api': 'http://localhost:5000'
-    }
-  },
+export default defineConfig({  
+  base: './', 
   plugins: [react()],
 })
